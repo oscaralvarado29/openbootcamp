f7d53cc9437f95423bc142435ca3c2d4773260b0
oscaralvarado29 <oscar.alvarado.mt@correo.usa.edu.co>
2022-08-20 19:59:22 -0500

    solucion ejercicio

---
+++ ejercicio 3/src/solucion/Coche.java
@@ -0,0 +1,10 @@
+package solucion;
+
+public class Coche {
+    public int numeroDePuertas = 4;
+
+    public void sumarPuertas(){
+
+        this.numeroDePuertas++;
+    }
+}
---
+++ ejercicio 3/src/solucion/EjercicioTres.java
@@ -0,0 +1,21 @@
+package solucion;
+
+public class EjercicioTres {
+
+    public static void main(String[] args) {
+        suma(20, 40, 60);
+
+        Coche miCoche = new Coche();
+
+        miCoche.sumarPuertas();
+        miCoche.sumarPuertas();
+        System.out.println("El numero de puertas es: " + miCoche.numeroDePuertas);
+
+    }
+
+    public static void suma(int a, int b, int c){
+        int resultado;
+        resultado = a + b + c;
+        System.out.println(resultado);
+    }
+}
